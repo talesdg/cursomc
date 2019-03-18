e5da16cf7937780914a341dd893a839b3da3f11d
Tales Dias <miletotg@Gmail.com>
2019-03-17 22:23:09 -0300

    Testando a primeira Categoria

--- src/main/java/com/talesdias/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/talesdias/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,26 @@
 package com.talesdias.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.talesdias.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String lista() {
-		return "Rest está funcionando";
+	public List<Categoria> lista() {
+		Categoria cat1 = new Categoria(1,"Informatica");
+		Categoria cat2 = new Categoria(1,"Escritorio");
+		
+		List<Categoria> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		return list;
 	}
 }
