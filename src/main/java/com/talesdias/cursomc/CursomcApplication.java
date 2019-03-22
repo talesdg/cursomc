a9886fc7b7dae44d1f12960e550aeb3c8f069fe0
Tales Dias <miletotg@Gmail.com>
2019-03-21 23:13:31 -0300

    Estado e cidade

--- src/main/java/com/talesdias/cursomc/CursomcApplication.java
+++ src/main/java/com/talesdias/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.talesdias.cursomc.domain.Categoria;
+import com.talesdias.cursomc.domain.Cidade;
+import com.talesdias.cursomc.domain.Estado;
 import com.talesdias.cursomc.domain.Produto;
 import com.talesdias.cursomc.repositories.CategoriaRepository;
+import com.talesdias.cursomc.repositories.CidadeRepository;
+import com.talesdias.cursomc.repositories.EstadoRepository;
 import com.talesdias.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -33,6 +43,7 @@ public class CursomcApplication implements CommandLineRunner{
 		Produto p1 = new Produto(null, "Computador", 2000.0);
 		Produto p2 = new Produto(null, "Impressora", 800.0);
 		Produto p3 = new Produto(null, "Mouse", 30.0);
+		
 
 		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
@@ -43,6 +54,19 @@ public class CursomcApplication implements CommandLineRunner{
 
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia",est1);
+		Cidade c2 = new Cidade(null, "São Paulo",est2);
+		Cidade c3 = new Cidade(null, "Campinas",est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 	}
 
 }
---
+++ src/main/java/com/talesdias/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,9 @@
+package com.talesdias.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.talesdias.cursomc.domain.Cidade;
+
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
